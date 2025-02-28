907eaacc60935bc858aad72f1102d53dbb9f5f7c
Alina Rohozinska <arohozi@tlu.ee>
2025-02-28 11:01:59 +0200

    kodutöö

---
+++ 01_07.02_andmemudelid/kymnevoistlus/src/main/java/ee/arohozi/kymnevoistlus/repository/RaceRepository.java
@@ -0,0 +1,7 @@
+package ee.arohozi.kymnevoistlus.repository;
+
+import ee.arohozi.kymnevoistlus.entity.Race;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RaceRepository extends JpaRepository<Race, Long> {
+}
