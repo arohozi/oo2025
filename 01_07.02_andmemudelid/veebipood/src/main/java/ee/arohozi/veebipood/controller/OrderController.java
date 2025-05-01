31d1ce6f4c031b2cfd0ac9ea839609e1cd353ba8
Alina Rohozinska <arohozi@tlu.ee>
2025-05-01 23:47:53 +0300

    Updated veebipood

--- 01_07.02_andmemudelid/veebipood/src/main/java/ee/arohozi/veebipood/controller/OrderController.java
+++ 01_07.02_andmemudelid/veebipood/src/main/java/ee/arohozi/veebipood/controller/OrderController.java
@@ -4,29 +4,38 @@ import ee.arohozi.veebipood.entity.Order;
 import ee.arohozi.veebipood.entity.Product;
 import ee.arohozi.veebipood.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin(origins ="http://localhost:5173")
 @RestController
 public class OrderController {
 
     @Autowired
     OrderRepository orderRepository;
 
+    @GetMapping("orders")
+    public List<Order> getOrders() {
+        return orderRepository.findAll();
+    }
+
     @PostMapping("orders")
     public List<Order> addOrder(@RequestBody Order order) {
         order.setCreated(new Date());
         double sum = 0;
-        for (Product product : order.getProducts()) {
-            sum += product.getPrice();
+        for (Product p: order.getProducts()) {
+            sum = sum + p.getPrice();
+            // sum += p.getPrice();
         }
+//        for (int i = 0; i < order.getProducts().size(); i++) {
+//            Product p = order.getProducts().get(i);
+//            sum = sum + p.getPrice();
+////             sum += p.getPrice();
+//        }
         order.setTotalSum(sum);
         orderRepository.save(order);
         return orderRepository.findAll();
     }
 }
-
--- 01_07.02_andmemudelid/veebipood/src/main/java/ee/arohozi/veebipood/controller/ProductController.java
+++ 01_07.02_andmemudelid/veebipood/src/main/java/ee/arohozi/veebipood/controller/ProductController.java
@@ -3,33 +3,44 @@ package ee.arohozi.veebipood.controller;
 import ee.arohozi.veebipood.entity.Product;
 import ee.arohozi.veebipood.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
 
+    // Autowired all joon, mis soovitab kasutada seda (aga teeb sama välja):
+//    public ProductController(ProductRepository productRepository) {
+//        this.productRepository = productRepository;
+//    }
+
+    // localhost:8080/products
     @GetMapping("products")
     public List<Product> getProducts() {
-        return productRepository.findAll();
+        return productRepository.findAll(); // [] -> SELECT * FROM   extends JpaRepository<Product>
     }
 
-    @PostMapping("products")
+    @PostMapping("products") // POSTMAN rakendus
     public List<Product> addProduct(@RequestBody Product product) {
         if (product.getId() != null) {
-            throw new RuntimeException("err_product_id_not_null");
+            throw new RuntimeException("ERROR_CANNOT_ADD_WITH_ID");
         }
         if (product.getPrice() <= 0) {
-            throw new RuntimeException("err_product_price_negative");
+            throw new RuntimeException("ERROR_PRICE_MUST_BE_POSITIVE");
         }
-        productRepository.save(product);
+        productRepository.save(product);  // INSERT INTO products
         return productRepository.findAll();
     }
 
+    // DELETE localhost:8080/products/1
     @DeleteMapping("products/{id}")
     public List<Product> deleteProduct(@PathVariable Long id) {
         productRepository.deleteById(id);
@@ -39,10 +50,10 @@ public class ProductController {
     @PutMapping("products")
     public List<Product> editProduct(@RequestBody Product product) {
         if (product.getId() == null) {
-            throw new RuntimeException("err_product_id_null");
+            throw new RuntimeException("ERROR_CANNOT_EDIT_WITHOUT_ID");
         }
         if (product.getPrice() <= 0) {
-            throw new RuntimeException("err_product_price_negative");
+            throw new RuntimeException("ERROR_PRICE_MUST_BE_POSITIVE");
         }
         productRepository.save(product);
         return productRepository.findAll();
@@ -53,25 +64,81 @@ public class ProductController {
         return productRepository.findById(id).orElseThrow();
     }
 
-    @PatchMapping("products")
-    public List<Product> editProductValue(@RequestParam Long id, @RequestParam String field, @RequestParam String value) {
+    // kui on 1 on ilusam kasutada @PathVariable
+    // kui on 2 või enam parameetrit peaks kasutama RequestParam
+    // localhost:8080/products/4/name/Aura
+    @PatchMapping("products") // PATCH localhost:8080/products?id=4&field=name&value=Aura
+    public List<Product> editProductValue(@RequestParam Long id, String field, String value) {
         if (id == null) {
-            throw new RuntimeException("err_product_id_null");
+            throw new RuntimeException("ERROR_CANNOT_EDIT_WITHOUT_ID");
         }
         Product product = productRepository.findById(id).orElseThrow();
-
         switch (field) {
             case "name" -> product.setName(value);
             case "price" -> {
                 if (Double.parseDouble(value) <= 0) {
-                    throw new RuntimeException("err_product_price_negative");
+                    throw new RuntimeException("ERROR_PRICE_MUST_BE_POSITIVE");
                 }
                 product.setPrice(Double.parseDouble(value));
             }
             case "image" -> product.setImage(value);
             case "active" -> product.setActive(Boolean.parseBoolean(value));
         }
+// String ei ole primitiivne väärtus. Seega ei saa tema väärtusi kontrollida võrdusmärgiga
+//        if (field.equals("name")) {
+//            product.setName(value);
+//        } else if (field.equals("price")) {
+//            product.setPrice(Double.parseDouble(value));
+//        } else if (field.equals("image")) {
+//            product.setImage(value);
+//        } else if (field.equals("active")) {
+//            product.setActive(Boolean.parseBoolean(value));
+//        }
         productRepository.save(product);
         return productRepository.findAll();
     }
+
+    // const filteredProducts = [];
+
+//    @GetMapping("/category-products")
+//    public List<Product> getCategoryProducts(@RequestParam Long categoryId) {
+//        List<Product> products = productRepository.findAll();
+//        List<Product> filteredProducts = new ArrayList<>(); // tühi List
+    ////        for (int i = 0; i < products.size(); i++) {
+    ////             if (products.get(i).getCategory().getId().equals(categoryId)) {
+    ////                  filteredProducts.add(products.get(i));
+    ////              }
+    ////        }
+//        for (Product p: products) {
+//            // ==   --> kontrollib, kas vasak pool ja parem pool on identsed
+//            // .equals --> kontrollib, kas vasaku ja parema poole väärtused on identsed
+//            if (p.getCategory().getId().equals(categoryId)) {
+//                filteredProducts.add(p);
+//            }
+//        }
+//        return filteredProducts;
+//    }
+
+    // localhost:8080/category-products?categoryId=1&page=0&size=2&sort=name,asc
+    // localhost:8080/category-products?categoryId=1&page=0&size=2&sort=price,desc
+    @GetMapping("/category-products")
+    public Page<Product> getCategoryProducts(@RequestParam Long categoryId, Pageable pageable) {
+        if (categoryId == -1) {
+            return productRepository.findAll(pageable); // returniga funktsioon lõppeb, else blokki pole vaja
+        }
+        return productRepository.findByCategory_Id(categoryId, pageable);
+    }
 }
+
+
+// 1xx --> informatiivsed - meie ei kasuta
+// 2xx --> edukad  200   201(created)
+// 3xx --> suunamine - meie ei kasuta
+// 4xx --> päringu tegija veaga (front-end viga).   client error
+//       400 - üldine viga
+//       401, 403 - autentimisega seotud vead
+//       402 - maksetega seotud vead
+//       404 - api endpoint on vale
+//       405 - Method not allowed
+//       415 - sisu tüüp on vale
+// 5xx --> back-end viga.  500
